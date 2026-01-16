efca941edb68cf94e873307b18ae932327e59958
Om Das <omdasg11@gmail.com>
2026-01-16 15:39:58 +0530

    Create build-about-timeline.js

---
+++ scripts/build-about-timeline.js
@@ -0,0 +1,20 @@
+/**
+ * build-about-timeline.js
+ *
+ * Generator for About page timeline.
+ *
+ * Input:
+ *   /data/about/timeline.yaml
+ *
+ * Output:
+ *   /data/about/timeline.json
+ *
+ * This file will:
+ * - Read human-written YAML
+ * - Validate milestone entries
+ * - Convert them into machine-safe JSON
+ *
+ * IMPLEMENTATION PENDING
+ */
+
+console.log("About timeline generator placeholder.");
