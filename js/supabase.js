f1c77ae55a1487fde66e2fe2ab704aef1e903324
Om Das <omdasg11@gmail.com>
2026-01-29 13:31:56 +0530

    Add supabase.js file

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://jigeofftrhhyvnjpptxw.supabase.co",
+  "sb_publishable_nwdMKnjcV_o-WSe_VMs9CQ_xpaMeGAT"
+);
